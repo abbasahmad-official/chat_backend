3b03468df8f6b23721d8b542c44943b6d22781db
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-08-24 21:40:03 +0500

    cors update

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://chat-frontend-eight-vert.vercel.app'
+}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 // OpenAI configuration
